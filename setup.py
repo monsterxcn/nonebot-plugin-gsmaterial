c60fd16ff6f535cb5b5934f98aa65040c739bc89
monsterxcn <monsterxcn@gmail.com>
2023-08-17 09:28:21 +0800

    :wrench: 更新须弥周本等配置 (release v0.2.6)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nonebot-plugin-gsmaterial",
-    version="0.2.5",
+    version="0.2.6",
     author="monsterxcn",
     author_email="monsterxcn@gmail.com",
     description="Genshin daily material plugin for NoneBot2",
